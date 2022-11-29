93ba59e50d22dd24c351c21632abb51dd2acdfc5
Antonio Leo <antonioln00@gmail.com>
2022-11-29 17:49:23 -0300

    Alteração de nome de arquivo de script para array and loops e execução de exercicios feito em momento síncrono

---
+++ array-and-loops.js
@@ -0,0 +1,18 @@
+const lotteryNumbers = [14,26,56,39,27,5, 30, 18, 58];
+// console.log(lotteryNumbers[0]);
+// console.log(lotteryNumbers[1]);
+// console.log(lotteryNumbers[2]);
+// console.log(lotteryNumbers[3]);
+// console.log(lotteryNumbers[4]);
+// console.log(lotteryNumbers[5]);
+
+console.log(lotteryNumbers.length);
+
+// for (let index = 0; index < lotteryNumbers.length; index += 1) {
+//     // console.log(lotteryNumbers[index]);
+//     console.log(index);
+// }
+
+for (value of lotteryNumbers) {
+    console.log(value);
+}
