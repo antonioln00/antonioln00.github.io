97cc6903eeb1248206b1db4e4b367e1ddb175207
Antonio Leo <antonioln00@gmail.com>
2022-11-29 21:03:25 -0300

    Execução dos exercicios 1 ao 3

---
+++ script.js
@@ -0,0 +1,23 @@
+const a = 20;
+const b = 17;
+const c = 45
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+if (a > b) {
+    console.log("O número a é maior do que b!");
+} else {
+    console.log("O numero b é maior do que a!");
+};
+
+if (a > b && a > c) {
+    console.log ("o maior numero é o " + a + "(a)!")
+} else if (b > a && b > c) {
+    console.log ("o maior numero é o " + b + "(b)!")
+} else {
+    console.log ("o maior número é o " + c + " (c)!")
+};
