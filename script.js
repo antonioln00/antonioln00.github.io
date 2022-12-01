3185b5e2f7138e164ef840b0cf1266eee8068650
Antonio Leo <antonioln00@gmail.com>
2022-12-01 09:52:27 -0300

    Execução exercicio 3 e 4

---
+++ script.js
@@ -0,0 +1,29 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let index = 0; index <= numbers.length; index += 1){
+//     console.log(index);
+// }
+
+// let result = 0;
+// for (let index = 0; index < numbers.length; index +=  1){
+//     result += numbers[index];
+// }
+// console.log(result)
+
+// let sum = 0;
+
+// for (let value = 0; value < numbers.length; value +=1) {
+//     sum += numbers[value];
+// }
+
+// let average = sum / numbers.length
+// console.log(average);
+
+// if (average > 20) {
+//     console.log("valor maior que 20")
+// } else {
+//     console.log("valor menor ou igual a 20")
+// }
+
+
+
