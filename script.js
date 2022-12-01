62a88ba61f94ca25419f08ab60e2e30d6e286a4b
Antonio Leo <antonioln00@gmail.com>
2022-11-30 21:26:08 -0300

    Execução exercicio 7

--- script.js
+++ script.js
@@ -91,3 +91,20 @@
 
 // }
 
+let nota;
+
+if (nota < 100 && nota >= 90) {
+    console.log("A")
+} else if (nota < 90 && nota >= 80) {
+    console.log("B")
+} else if (nota < 80 && nota >= 70) {
+    console.log("C")
+} else if (nota < 70 && nota >= 60) {
+    console.log ("D")
+} else if (nota < 60 && nota >= 50) {
+    console.log ("E")
+} else if (nota < 50 && nota > 0) {
+    console.log("F")
+} else {
+    console.log("Erro")
+}
