51205c3126a1a202c34715b8af8b1e4f031baa5f
Antonio Leo <antonioln00@gmail.com>
2022-12-01 22:06:39 -0300

    Execução exercicio 2

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-let fatorial = 1;
+// let fatorial = 1;
 
-for (index = 10; index > 0; index -=1){
-    fatorial *= index;
-}
+// for (index = 10; index > 0; index -=1){
+//     fatorial *= index;
+// }
 
-console.log(fatorial);
+// console.log(fatorial);
+
+// let word = 'tryber';
+// let reverseWord = "";
+
+// for (let index = 0; index < word.length; index += 1){
+//     reverseWord += word[word.length - 1 - index];
+// }
+
+// console.log(reverseWord)
+
+// let word2 = 'Antonio';
+// let reverseWord2 = "";
+
+// for (let index2 = 0; index2 < word2.length; index2 += 1){
+//     reverseWord2 += word2[word2.length - 1 - index2];
+// }
+
+// console.log(reverseWord2)
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
